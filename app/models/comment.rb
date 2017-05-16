7ed45b42a0f9c44cd6623ebde4cb018e215fa89c
nurami <mrnursil@gmail.com>
2017-05-16 14:19:56 +0300

    add dependencies between product and comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :product
+
+  validates :text, precence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  has_many :comments, dependent: :destroy
+
+  validate :title, :description, :amount, :price, presence: true
 end
