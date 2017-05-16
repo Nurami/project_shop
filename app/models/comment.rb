b5d1af51d55df5b9c6a44d8c8f18a8e14b1f5dd8
nurami <mrnursil@gmail.com>
2017-05-16 14:28:19 +0300

    change comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :product
 
-  validates :text, precence: true
+  validates :text, presence: true
+
+  def created_at_h
+    created_at.strftime('%B %d, %Y at %I:%M%p')
+  end
 end
