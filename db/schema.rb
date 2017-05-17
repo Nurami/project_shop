a345e8e3f7bc651da809a0d543e7fd3ce4a18dbf
nurami <mrnursil@gmail.com>
2017-05-17 20:54:13 +0300

    add ordered_product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170517173823) do
+ActiveRecord::Schema.define(version: 20170517175327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170517173823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
+  end
+
+  create_table "ordered_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
---
+++ spec/factories/ordered_products.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :ordered_product do
+    product_id 1
+    order_id 1
+  end
+end
