7ddad61a4a021d3ccf7d99444297e6b912606aef
nurami <mrnursil@gmail.com>
2017-05-17 21:40:23 +0300

    change product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
 
+  has_many :ordered_products
+  has_many :orders, through: :ordered_products
+
   validates :title, :description, :amount, :price, presence: true
 end
