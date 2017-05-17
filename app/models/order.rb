81c19d68eed601b6401eea0f50353fcd3948ccb3
nurami <mrnursil@gmail.com>
2017-05-17 21:36:04 +0300

    change order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ApplicationRecord
+  belongs_to :user
+
+  STATUSES = {
+    "in progress" => "Выполняется",
+    "done" => "Выполнен",
+    "archived" => "Архивный"
+  }
+
+  validates :status, inclusion: STATUSES.keys
+
+  has_many :ordered_product
+  has_many :product, through: :ordered_product
 end
