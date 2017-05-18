40b88dd227e63894863a37aac79f90b6e5084262
nurami <mrnursil@gmail.com>
2017-05-18 18:32:35 +0300

    change orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,32 @@
-class OrdersController < InheritedResources::Base
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  expose_decorated :order
+  expose_decorated :orders, -> { fetch_orders }
+
+  def new
+    initialize_order
+    empty_cart
+
+    redirect_to orders_path
+  end
+
+  def index; end
+
+  def show; end
 
   private
 
-    def order_params
-      params.require(:order).permit(:user_id, :status)
-    end
-end
+  def fetch_orders
+    current_user.orders.order("created_at ASC")
+  end
 
+  def initialize_order
+    order = current_user.orders.create status: "in_progress"
+    order.products << Product.where(id: session[:cart])
+  end
+
+  def empty_cart
+    session[:cart] = []
+  end
+end
